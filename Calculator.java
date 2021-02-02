1e58d64196fb2cf0287bacc5b4130ec40f02b202
IgnasKia <78438345+IgnasKia@users.noreply.github.com>
2021-02-02 21:23:20 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,53 +3,33 @@ import java.util.*;
 
 public class Calculator {
 
+    static int x,y, z;
+    static Scanner myObj = new Scanner(System.in);
+
     public static void main(String[] args){
 
         calculate();
+
     }
 
-    static void calculate(){
+    public static void calculate(){
         String yesNo;
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter figure(rectangle,triangle,square)");
         String figure = myObj.nextLine();  // Read user input
-        int x, y, z;
+
         String choice;
         boolean isRunning = true;
         while(isRunning) {
             switch (figure) {
                 case "rectangle":
-                    System.out.println("Enter figure values");
-                    x = myObj.nextInt();
-                    y = myObj.nextInt();
-                    if (x == 0 || y == 0) {
-                        System.out.println("Rectangle line value can't be 0");
-                    } else if (x < 0 || y < 0) {
-                        System.out.println("Rectangle line value can't be negative");
-                    } else
-                        System.out.println("Rectangle perimeter is " + (x * 2 + y * 2) + " and area is " + (x * y));
+                    calculateRectangle();
                     break;
                 case "triangle":
-                    System.out.println("Enter figure values");
-                    x = myObj.nextInt();
-                    y = myObj.nextInt();
-                    z = myObj.nextInt();
-                    int s = (x + y + z) / 2;
-                    double area = Math.sqrt(s * (s - x) * (s - y) * (s - z));
-                    if (x == 0 || y == 0 || z == 0) {
-                        System.out.println("Triangle line value can't be 0");
-                    } else if (x < 0 || y < 0) {
-                        System.out.println("Triangle line value can't be negative");
-                    } else
-                        System.out.println("Triangle perimeter is " + (x + y + z) + " and area is " + area);
+                    calculateTriangle();
                     break;
                 case "square":
-                    System.out.println("Enter figure values");
-                    x = myObj.nextInt();
-                    if (x <= 0) {
-                        System.out.println("Square line value can't be 0 or negative");
-                    } else
-                        System.out.println("Square perimeter is " + (x * 4) + " and area is " + (x * x));
+                    calculateSquare();
                     break;
                 default:
                     System.out.println("Error: bad input");
@@ -68,5 +48,38 @@ public class Calculator {
             }
 
         }
+    }
+    public static void calculateRectangle(){
+        System.out.println("Enter figure values");
+        x = myObj.nextInt();
+        y = myObj.nextInt();
+        if (x == 0 || y == 0) {
+            System.out.println("Rectangle line value can't be 0");
+        } else if (x < 0 || y < 0) {
+            System.out.println("Rectangle line value can't be negative");
+        } else
+            System.out.println("Rectangle perimeter is " + (x * 2 + y * 2) + " and area is " + (x * y));
+    }
+    public static void calculateTriangle(){
+        System.out.println("Enter figure values");
+        x = myObj.nextInt();
+        y = myObj.nextInt();
+        z = myObj.nextInt();
+        int s = (x + y + z) / 2;
+        double area = Math.sqrt(s * (s - x) * (s - y) * (s - z));
+        if (x == 0 || y == 0 || z == 0) {
+            System.out.println("Triangle line value can't be 0");
+        } else if (x < 0 || y < 0) {
+            System.out.println("Triangle line value can't be negative");
+        } else
+            System.out.println("Triangle perimeter is " + (x + y + z) + " and area is " + area);
+    }
+    public static void calculateSquare(){
+        System.out.println("Enter figure values");
+        x = myObj.nextInt();
+        if (x <= 0) {
+            System.out.println("Square line value can't be 0 or negative");
+        } else
+            System.out.println("Square perimeter is " + (x * 4) + " and area is " + (x * x));
     }
 }
